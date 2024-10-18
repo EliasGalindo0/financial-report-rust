79214456f30b0a13a4589866611110d977d9eac6
EliasGalindo0 <eliasgalindo@gmail.com>
2024-10-17 22:49:18 -0300

    Chore: Remove unecessary coments

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,11 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let text = extract_text_from_mem(&bytes)?;
 
-    // Clean the text by removing unnecessary spaces in numbers
     let cleaned_text = remove_spaces_in_numbers(&text);
 
     let mut expenses: Vec<(String, String)> = Vec::new();
     let mut revenues: Vec<(String, String)> = Vec::new();
 
-    // Regex to capture the full transaction name and the value
     let expense_regex = Regex::new(
         r"((?:Compra no debito|Debito automatico|Pix enviado|Pagamento|Aplicacao).*?)\s*-R\$ ?([\d.,]+)",
     )
@@ -23,26 +21,22 @@ fn main() -> Result<(), Box<dyn Error>> {
     let revenue_regex =
         Regex::new(r"((?:Pix recebido|Cheque recebido|Credito).*?)\s*R\$ ?([\d.,]+)").unwrap();
 
-    // Extract expenses
     for cap in expense_regex.captures_iter(&cleaned_text) {
         let transaction_name = cap.get(1).map_or("", |m| m.as_str()).to_string();
         let value = cap.get(2).map_or("", |m| m.as_str()).to_string();
         expenses.push((transaction_name, value)); // Store value without "R$"
     }
 
-    // Extract revenues
     for cap in revenue_regex.captures_iter(&cleaned_text) {
         let transaction_name = cap.get(1).map_or("", |m| m.as_str()).to_string();
         let value = cap.get(2).map_or("", |m| m.as_str()).to_string();
         revenues.push((transaction_name, value)); // Store value without "R$"
     }
 
-    // Create CSV for expenses
     if !expenses.is_empty() {
         let file_name = "expenses.csv";
         let mut wtr = Writer::from_writer(File::create(file_name)?);
 
-        // Write each expense as a row in the CSV
         for (name, value) in expenses {
             wtr.write_record(&[value, name])?;
         }
@@ -50,12 +44,10 @@ fn main() -> Result<(), Box<dyn Error>> {
         println!("Expenses report generated successfully!");
     }
 
-    // Create CSV for revenues
     if !revenues.is_empty() {
         let file_name = "revenues.csv";
         let mut wtr = Writer::from_writer(File::create(file_name)?);
 
-        // Write each revenue as a row in the CSV
         for (name, value) in revenues {
             wtr.write_record(&[value, name])?;
         }
@@ -66,13 +58,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-// Function to remove spaces between numbers
 fn remove_spaces_in_numbers(text: &str) -> String {
     let re = Regex::new(r"(?P<number>\d[\d\s.,]*\d)").unwrap();
 
     re.replace_all(text, |caps: &regex::Captures| {
         let number = caps.name("number").unwrap().as_str();
-        number.replace(" ", "") // Remove all spaces within the number
+        number.replace(" ", "") 
     })
     .to_string()
 }
